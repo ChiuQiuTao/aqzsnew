73bad04f7ae0c52bd89178ebba6b148273393421
anblueliu <73168376@qq.com>
2019-03-07 10:22:48 +0800

    3.7

---
+++ js/index.js
@@ -0,0 +1,146 @@
+var menudata = [{
+        icon: "icon-ditu",
+        title: "GIS地图",
+        child: [{
+            url: "./src/AGIS/message.html",
+            title: "信息管理"
+        }, ]
+    }, {
+        icon: "icon-webicon06",
+        title: "网格管理",
+        child: [{
+                url: "./src/Bgrid/planning.html",
+                title: "网格规划"
+            }, {
+                url: "./src/Bgrid/task.html",
+                title: "任务指派"
+            },
+            {
+                url: "./src/Bgrid/Lawdo.html",
+                title: "执法信息"
+            },
+        ]
+    },
+    {
+        icon: "icon-fengxianyuan",
+        title: "风险等级",
+        child: [{
+            url: "./src/Crisk/risk.html",
+            title: "风险等级"
+        }]
+    },
+    {
+        icon: "icon-jichushujuguanli",
+        title: "溯源查询",
+        child: [{
+                url: "./src/Dtrace/goods.html",
+                title: "商品追溯"
+            }, {
+                url: "./src/Dtrace/Livestock.html",
+                title: "禽畜追溯"
+            },
+            {
+                url: "./src/Dtrace/farmproducts.html",
+                title: "农产品追溯"
+            },
+            {
+                url: "./src/Dtrace/product.html",
+                title: "产品追溯"
+            },
+        ]
+    },
+    {
+        icon: "icon-zhuanjiarenzheng",
+        title: "人才专家",
+        child: [{
+                url: "./src/Etalent/information.html",
+                title: "专家信息管理"
+            }, {
+                url: "./src/Etalent/advice.html",
+                title: "专家建议栏"
+            },
+            {
+                url: "./src/Etalent/interflow.html",
+                title: "信息交流区"
+            },
+        ]
+    },
+    {
+        icon: "icon-huaxuehecheng",
+        title: "食品安全",
+        child: [{
+            url: "./src/Ffool/security.html",
+            title: "安全标准"
+        }, {
+            url: "./src/Ffool/law.html",
+            title: "法律法规"
+        }, ]
+    },
+    {
+        icon: "icon-huanjingjiance",
+        title: "环境监控",
+        child: [{
+                url: "./src/Genvironment/data.html",
+                title: "环境数据查询"
+            }, {
+                url: "./src/Genvironment/monitoring.html",
+                title: "实时监控"
+            },
+
+        ]
+    },
+    {
+        icon: "icon-xinxi",
+        title: "信息预警",
+        child: [{
+                url: "./src/Hwarning/planting.html",
+                title: "种植预警"
+            }, {
+                url: "./src/Hwarning/farming.html",
+                title: "养殖预警"
+            },
+            {
+                url: "./src/Hwarning/enterprise.html",
+                title: "企业预警"
+            },
+        ]
+    },
+    {
+        icon: "icon-tiaochariji",
+        title: "日记管理",
+        child: [{
+                url: "./src/Idiary/login.html",
+                title: "企业登记日记"
+            }, {
+                url: "./src/Idiary/abnormal.html",
+                title: "异常日志管理"
+            },
+            {
+                url: "./src/Idiary/traffic.html",
+                title: "系统访问量统计"
+            },
+            {
+                url: "./src/Idiary/operation.html",
+                title: "操作日志"
+            },
+        ]
+    },
+
+    {
+        icon: "icon-shezhi",
+        title: "基础设置",
+        child: [{
+                url: "./src/Jbasis/permissions.html",
+                title: "权限设置"
+            },
+            {
+                url: "./src/Jbasis/use.html",
+                title: "使用帮助"
+            }, {
+                url: "./src/Jbasis/about.html",
+                title: "关于我们"
+            },
+        ]
+    },
+
+]
