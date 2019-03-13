9e915de6112f869b12f579c23da56e360d56fd1b
anblueliu <73168376@qq.com>
2019-03-13 19:32:52 +0800

    liu

--- js/apl.js
+++ js/apl.js
@@ -1,22 +1,21 @@
 var base = "http://192.168.163.100:9193/";
 var jq = jQuery.noConflict();
-var sessionStorage = window.sessionStorage;
+var sessions = getCookie('token');
 
 function ajax(url, param, type) {
-    var sessions = sessionStorage.getItem('token');
+
     if (sessions == "" && url != "login") {
         window.location.href = "../src/login.html";
         return;
     }
-    console.log(sessions)
-        // 利用了jquery延迟对象回调的方式对ajax封装，使用done()，fail()，always()等方法进行链式回调操作
+    // 利用了jquery延迟对象回调的方式对ajax封装，使用done()，fail()，always()等方法进行链式回调操作
     return jq.ajax({
         url: base + url,
         data: param || {},
         type: type || 'POST',
         async: true,
         beforeSend: function(xhr) {
-            xhr.setRequestHeader("Authorization", "Bearer" + " " + sessions)
+            xhr.setRequestHeader("Authorization", "Bearer" + " " + sessions);
         },
     });
 }
@@ -24,14 +23,19 @@ function ajax(url, param, type) {
 function handleAjax(url, param, type) {
     return ajax(url, param, type).then(function(resp) {
         // 成功回调
-        resp = JSON.parse(resp);
+        console.log(typeof resp);
+        if (typeof resp == 'string') {
+            resp = JSON.parse(resp)
+        }
         if (resp.status == 200) {
-            sessionStorage.setItem('token', resp.jwtToken);
+            setCookie('token', resp.jwtToken);
             return resp;
         }
+
         if (resp.code == 10000) {
             return resp; // 直接返回要处理的数据，作为默认参数传入之后done()方法的回调
         } else {
+
             return jq.Deferred().reject(resp); // 返回一个失败状态的deferred对象，把错误代码作为默认参数传入之后fail()方法的回调
         }
     }, function(err) {
@@ -52,4 +56,23 @@ function alerts(texts) {
         time: 1000,
     })
     return;
+}
+
+/*写cookie*/
+function setCookie(name, value) {
+    var Days = 1;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+}
+
+/*读cookie*/
+function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+
+    if (arr = document.cookie.match(reg))
+
+        return unescape(arr[2]);
+    else
+        return null;
 }
